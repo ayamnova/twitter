ad52f5cc3c4ab24d974c735b02e59ef920b0a548
Karsten Ladner <karstenladdy@gmail.com>
2018-07-16 14:39:55 -0500

    Modified to catch total tweets and English tweets

--- daily-totals.sh
+++ daily-totals.sh
@@ -5,8 +5,12 @@ dirs=$(find ./crisis/crisis/2018/05 -type d -maxdepth 1 -mindepth 1 | sort)
 # loop through all the days
 for directory in $dirs
 do
+   # find all the files
+   files=$(find $directory -type f -not -iname "*.tmp") 
    # find the total number of tweets
-   total=$(find $directory -type f -not -iname "*.tmp" | xargs cat | wc -l)
+   total=$(cat $files | wc -l)
+   # find the total number of English tweets
+   eng=$(jq '.lang' $files | grep en | wc -l)
    # print that to the daily total 
-   echo $directory,$total >> ./out/daily-totals.txt
+   echo $directory,$total,$eng >> ./out/daily-totals-test.txt
 done
