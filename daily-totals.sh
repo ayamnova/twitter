0437b2458d51f9dec8a7b1ade10ee646981e7da4
Karsten Ladner <karstenladdy@gmail.com>
2018-07-18 09:58:21 -0500

    Can process huge folders now

--- daily-totals.sh
+++ daily-totals.sh
@@ -1,15 +1,28 @@
 #! /bin/sh
 # find all the day folders in the given month
-dirs=$(find ./crisis/crisis/2018/05 -type d -maxdepth 1 -mindepth 1 | sort)
+#dirs=$(find ./crisis/crisis/2018/06 -type d -maxdepth 1 -mindepth 1 | sort)
 # loop through all the days
-for directory in $dirs
-do
+#for directory in $dirs
+#do
    # find all the files
-   files=$(find $directory -type f -not -iname "*.tmp") 
+   #files=$(find $directory -type f -not -iname "*.tmp") 
    # find the total number of tweets
-   total=$(cat $files | wc -l)
+   #total=$(cat $files | wc -l)
    # find the total number of English tweets
-   eng=$(jq '.lang' $files | grep en | wc -l)
+   #eng=$(jq '.lang' $files | grep en | wc -l)
    # print that to the daily total 
-   echo $directory,$total,$eng >> ./out/daily-totals-test.txt
-done
+   #echo $directory,$total,$eng >> ./out/daily-totals-test.txt
+#done
+directory=$1
+# files=$(find $directory -type f -not -iname "*.tmp") 
+# find the total number of tweets
+# total=$(cat $files | wc -l)
+# find the total number of English tweets
+# eng=$(jq '.lang' $files | grep en | wc -l)
+# print that to the daily total 
+total=$(find $directory -type f -not -iname "*.tmp" -exec cat {} + | wc -l)
+echo $total
+eng=$(find $directory -type f -not -iname "*.tmp" -exec jq '.lang' {} + | grep en | wc -l)
+echo $eng
+# echo $directory,$total,$eng >> ./out/daily-totals-test.txt
+echo $directory,$total,$eng
