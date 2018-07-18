44d1f7faf854aaae0091e3ac876cb39fe14ff9f1
Karsten Ladner <karstenladdy@gmail.com>
2018-07-18 09:56:00 -0500

    Changed output file to make use of constants

--- sent.py
+++ sent.py
@@ -7,17 +7,19 @@ Date: 6/25/2018
 '''
 
 import sys
+from os.path import join as jn
 from tweets import get_tweets, get_sentiment
-from constants import PATH
+from constants import PATH, OUT
 
 if __name__ == '__main__':
-    out = sys.argv[2]
+    out = jn(OUT, sys.argv[2])
     dirs = sys.argv[1].split(',')
 
     with open(out, 'w') as fout:
         for d in dirs:
             tweets = get_tweets([d], key='text', prefix=PATH)
             sent, total, mn, mx, neg, neut, pos = get_sentiment(tweets[0])
+            print("Sentiment Calculated")
             fout.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n".format(
                 d, sent, mn, mx, neg, neut, pos, total, tweets[1]))
     fout.close()
