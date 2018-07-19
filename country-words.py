bffeac14b815fff997a0d0295f1a6199ea933ec1
Karsten Ladner <karstenladdy@gmail.com>
2018-07-19 11:40:56 -0500

    Added a script to get all the tweets by specified country and save them for later

---
+++ country-words.py
@@ -0,0 +1,42 @@
+import sys
+import carmen
+from afinn import Afinn
+from os.path import join as jn
+from tweets import get_tweets
+from tweets import save_to_file as save 
+from constants import PATH, OUT, PROC
+
+
+if __name__ == '__main__':
+    dirs = [PATH]
+    d = {
+            "United States": [],
+            "United Kingdom": [],
+            "India": [],
+            "France": []
+            }
+    tweets, filt = get_tweets(dirs)
+
+    # initialize the location engine
+    r = carmen.get_resolver()
+    r.load_locations()
+
+    # look through every tweet and see if it's in a country of interest
+    for t in tweets:
+        try:
+            # try to find a location
+            loc = r.resolve_tweet(t)[1]
+            country = loc.country
+            # if it's a country of interest append it's text to the country's
+            # tweet list
+            if country in d:
+                if t.get("extended_tweet") is None:
+                    d[country].append(t["text"])
+                else:
+                    d[country].append(t["extended_tweet"]["full_text"])
+        except TypeError:
+            continue
+    # save each country into a separate file
+    for c in d:
+        save({'data': d[c]}, jn(PROC, c + ".dat"))
+
