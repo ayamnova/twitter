a72a8a6a53ea833c5507cae8e3c7d6cea305cb51
Karsten Ladner <karsten@pineapple.k4lad>
2018-09-04 09:34:38 -0500

    added some basic changes

--- wcloud.py
+++ wcloud.py
@@ -16,8 +16,14 @@ import wordcloud
 from tweets import load_values_from_file as load
 from constants import PROC, OUT
 
-if "wc" in sys.argv[1]:
+keys = ["chemical", "isis", "isil", "assad", "syria", "syrian", "refugee"]
+
+if "wcd" in sys.argv[1]:
+    d = sys.argv[1].strip(".dat")
+elif "wc" in sys.argv[1]:
     d = "clouds"
+elif "tx-" in sys.argv[1]:
+    d = sys.argv[1].strip(".dat")
 else:
     d = sys.argv[1].strip(".dat")
 
@@ -27,17 +33,30 @@ except FileExistsError:
     pass
 
 freq = load(jn(PROC, sys.argv[1]))
+for k in keys:
+    del freq['data'][k]
 
-sten = np.array(Image.open(jn(PROC, 'fleeing.png')))
+# sten = np.array(Image.open(jn(PROC, 'fleeing.png')))
+wc = wordcloud.WordCloud(
+        height=400,
+        width=800,
+        scale=1.0,
+        max_words=250,
+        background_color="white",
+        )
+wc.generate_from_frequencies(freq['data'])
+wc.to_file(jn(OUT, d + "/", sys.argv[1].strip(".dat") + '.png'))
 
+'''
 for keyword, counts in freq['data'].items():
     wc = wordcloud.WordCloud(
-            height=1000,
-            width=1000,
+            height=400,
+            width=800,
             scale=1.0,
             max_words=250,
             background_color="white",
-            mask=sten
             )
+# mask=sten
     wc.generate_from_frequencies(counts)
     wc.to_file(jn(OUT, d + "/", keyword + '.png'))
+'''
